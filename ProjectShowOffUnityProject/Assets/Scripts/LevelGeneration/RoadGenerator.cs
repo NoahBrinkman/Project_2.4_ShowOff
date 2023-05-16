9022d9e32a1c9fd8895be87c089e3ccf33a10a3a
Kamila <kamila.matuszak@list.pl>
2023-05-16 15:12:40 +0200

    Road regenerate when player moves forward

--- ProjectShowOffUnityProject/Assets/Scripts/LevelGeneration/RoadGenerator.cs
+++ ProjectShowOffUnityProject/Assets/Scripts/LevelGeneration/RoadGenerator.cs
@@ -36,63 +36,64 @@ public class RoadGenerator : MonoBehaviour
             generateNewPiece = false;
         }
 
-        
-            RemoveRoad();
-        
+        if (_activePieces.Count <= piecesGeneratedAtOnce)
+        {
+            GenerateRoad();
+        }
+
+
+        RemoveRoad();
     }
 
     private void GenerateRoad()
     {
-        throw new NotImplementedException();
+        int randomRoad = Random.Range(0, 3);
+        _startPosition = _activePoints.AssetEnd;
+        float yDirection = 0;
+        float yDirectionT = 0;
+        bool isTPiece = false;
+        switch (_activePoints.Direction)
+        {
+            case 2:
+                //left
+                yDirection = 90 + _activePiece.transform.eulerAngles.y;
+                break;
+            case 3:
+                //right
+                yDirection = -90 + _activePiece.transform.eulerAngles.y;
+                break;
+            case 4:
+                //tpose
+                isTPiece = true;
+                yDirection = 90 + _activePiece.transform.eulerAngles.y;
+                yDirectionT = -90 + _activePiece.transform.eulerAngles.y;
+                break;
+            default: //straight 🤢
+                yDirection = 0 + _activePiece.transform.eulerAngles.y;
+                break;
+        }
+
+        Quaternion rotation = Quaternion.Euler(-90, 0, yDirection);
+        _activePiece = Instantiate(roadPieces[randomRoad].gameObject, _startPosition, rotation);
+        _activePieces.Add(_activePiece);
+        _activePoints = _activePieces[^1].GetComponent<RoadPoints>();
+            
+        // if (isTPiece)
+        // {
+        //     _startPosition = _activePoints.AssetLeft;
+        //     Quaternion rotationT = Quaternion.Euler(-90, 0, yDirectionT);
+        //     _activePiece = Instantiate(roadPieces[randomRoad].gameObject, _startPosition, rotationT);
+        //     _activePiece.name = $"road{i}";
+        //     _activePieces.Add(_activePiece);
+        //     _activePoints = _activePieces[^1].GetComponent<RoadPoints>();
+        // }
     }
 
     private void GenerateStartRoads()
     {
-
         for (int i = 0; i < piecesGeneratedAtOnce - 1; i++)
         {
-            int randomRoad = Random.Range(0, 3);
-            _startPosition = _activePoints.AssetEnd;
-            Debug.Log($"Start position: {_startPosition}");
-            float yDirection = 0;
-            float yDirectionT = 0;
-            bool isTPiece = false;
-            switch (_activePoints.Direction)
-            {
-                case 2:
-                    //left
-                    yDirection = 90 + _activePiece.transform.eulerAngles.y;
-                    break;
-                case 3:
-                    //right
-                    yDirection = -90 + _activePiece.transform.eulerAngles.y;
-                    break;
-                case 4:
-                    //tpose
-                    isTPiece = true;
-                    yDirection = 90 + _activePiece.transform.eulerAngles.y;
-                    yDirectionT = -90 + _activePiece.transform.eulerAngles.y;
-                    break;
-                default: //straight 🤢
-                    yDirection = 0 + _activePiece.transform.eulerAngles.y;
-                    break;
-            }
-            Quaternion rotation = Quaternion.Euler(-90,0,yDirection);
-            _activePiece = Instantiate(roadPieces[randomRoad].gameObject, _startPosition , rotation);
-            _activePiece.name = $"road{i}";
-            
-            if (isTPiece)
-            {
-                _startPosition = _activePoints.AssetLeft;
-                Quaternion rotationT = Quaternion.Euler(-90,0,yDirectionT);
-                _activePiece = Instantiate(roadPieces[randomRoad].gameObject, _startPosition , rotationT);
-                _activePiece.name = $"road{i}";
-                _activePieces.Add(_activePiece);
-                _activePoints = _activePieces[^1].GetComponent<RoadPoints>();
-            }
-            
-            _activePieces.Add(_activePiece);
-            _activePoints = _activePieces[^1].GetComponent<RoadPoints>();
+            GenerateRoad();
         }
     }
 
@@ -102,8 +103,6 @@ public class RoadGenerator : MonoBehaviour
         {
             Destroy(_activePieces[0]);
             _activePieces.RemoveAt(0);
-            
         }
-        
     }
 }
