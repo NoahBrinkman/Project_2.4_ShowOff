6a9f1bdf3145a0f48204be8d0f082fc55374d34b
Kamila <kamila.matuszak@list.pl>
2023-05-26 15:27:38 +0200

    Objects are spawned correctly with all rotations

--- ProjectShowOffUnityProject/Assets/Scripts/LevelGeneration/ObstaclesSpawn.cs
+++ ProjectShowOffUnityProject/Assets/Scripts/LevelGeneration/ObstaclesSpawn.cs
@@ -23,26 +23,34 @@ namespace LevelGeneration
         private void OnEnable()
         {
             _roadRenderer = GetComponent<Renderer>();
+            Vector3 pointFront;
+            Vector3 pointBack;
+            Vector3 pointLeft;
+            Vector3 pointRight;
+            float positionX = transform.position.x;
+            float positionY = _roadRenderer.bounds.max.y;
+            float positionZ = transform.position.z;
+            int modifier = 1;
+
+            int roadRotation = ((int)transform.localRotation.eulerAngles.y + 360) % 360;
 
             for (int i = 0; i <= 4; i += 2)
             {
-                Vector3 point1 = new Vector3(transform.position.x + (spacing - 0.5f) + i,
-                    _roadRenderer.bounds.max.y,
-                    transform.position.z);
-                Vector3 point2 = new Vector3(transform.position.x + (spacing + 0.5f) + i,
-                    _roadRenderer.bounds.max.y,
-                    transform.position.z);
-                Vector3 point3 = new Vector3(transform.position.x + spacing + i,
-                    _roadRenderer.bounds.max.y,
-                    transform.position.z - widthOffset);
-                Vector3 point4 = new Vector3(transform.position.x + spacing + i,
-                    _roadRenderer.bounds.max.y,
-                    transform.position.z + widthOffset);
-
-                _areasPoints.Add(point1);
-                _areasPoints.Add(point2);
-                _areasPoints.Add(point3);
-                _areasPoints.Add(point4);
+                switch (roadRotation)
+                {
+                    case 0:
+                        GeneratePointsVertical(positionX, 1, i, positionY, positionZ);
+                        break;
+                    case 90:
+                        GeneratePointsHorizontal(positionX, positionY, positionZ, -1, i);
+                        break;
+                    case 180:
+                        GeneratePointsVertical(positionX, -1, i, positionY, positionZ);
+                        break;
+                    case 270:
+                        GeneratePointsHorizontal(positionX, positionY, positionZ, 1, i);
+                        break;
+                }
             }
 
             int random = Random.Range(1, 30) % 3 + 1;
@@ -50,12 +58,36 @@ namespace LevelGeneration
             // 0 1 2 3 || 4 5 6 7 || 8 9 10 11
             for (int i = 0; i < random; i++)
             {
-                GameObject obstacle = Instantiate(obstaclesToAvoid[Random.Range(0,obstaclesToAvoid.Count-1)], transform);
+                GameObject obstacle = Instantiate(obstaclesToAvoid[Random.Range(0, obstaclesToAvoid.Count - 1)],
+                    transform);
                 _chosenObstacle = obstacle.GetComponent<Obstacle>().ObstacleType;
                 obstacle.transform.position = RandomPositionInTheArea(_areasPoints[4 * i], _areasPoints[4 * i + 1],
-                    _areasPoints[4 * i + 3],
-                    _areasPoints[4 * i + 2], _chosenObstacle);
+                    _areasPoints[4 * i + 3], _areasPoints[4 * i + 2], _chosenObstacle);
             }
+        }
+
+        private void GeneratePointsHorizontal(float x, float y, float z, int modifier, int offset)
+        {
+            var pointFront = new Vector3(x, y, z + (spacing - 0.5f) * modifier + offset * modifier);
+            var pointBack = new Vector3(x, y, z + (spacing + 0.5f) * modifier + offset * modifier);
+            var pointRight = new Vector3(x - widthOffset, y, z + spacing * modifier + offset * modifier);
+            var pointLeft = new Vector3(x + widthOffset, y, z + spacing * modifier + offset * modifier);
+            _areasPoints.Add(pointFront);
+            _areasPoints.Add(pointBack);
+            _areasPoints.Add(pointRight);
+            _areasPoints.Add(pointLeft);
+        }
+
+        private void GeneratePointsVertical(float x, int modifier, int offset, float y, float z)
+        {
+            Vector3 pointFront = new Vector3(x + (spacing - 0.5f) * modifier + offset * modifier, y, z);
+            Vector3 pointBack = new Vector3(x + (spacing + 0.5f) * modifier + offset * modifier, y, z);
+            Vector3 pointRight = new Vector3(x + spacing * modifier + offset * modifier, y, z - widthOffset);
+            Vector3 pointLeft = new Vector3(x + spacing * modifier + offset * modifier, y, z + widthOffset);
+            _areasPoints.Add(pointFront);
+            _areasPoints.Add(pointBack);
+            _areasPoints.Add(pointRight);
+            _areasPoints.Add(pointLeft);
         }
 
         /// <summary>
@@ -73,7 +105,7 @@ namespace LevelGeneration
             float newX = 0;
             float newZ = 0;
             float newY = 0;
-            
+
             float randomX = Random.Range(startPosition.x, endPosition.x);
             switch (obstacleType)
             {
@@ -96,8 +128,9 @@ namespace LevelGeneration
                     newY = startPosition.y + heightOffset;
                     break;
             }
+
             Vector3 newPosition = new Vector3(newX, newY, newZ);
-            
+
             return newPosition;
         }
     }
