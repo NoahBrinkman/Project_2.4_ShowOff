06d40bf0b8d71fc3c6fcfbd06dae40193976a5c8
Kamila <kamila.matuszak@list.pl>
2023-06-05 10:45:56 +0200

    Added vertical scene to the leaderboard

--- ProjectShowOffUnityProject/Assets/2. Scripts/UI/LeaderboardName.cs
+++ ProjectShowOffUnityProject/Assets/2. Scripts/UI/LeaderboardName.cs
@@ -8,13 +8,16 @@ using UnityEngine.UI;
 
 public class LeaderboardName : MonoBehaviour
 {
-    [SerializeField] private float shakePower;
-    [SerializeField] private float shakeTime;
-    [SerializeField] private float scale;
+    [SerializeField] private float shakePower = 5f;
+    [SerializeField] private float shakeTime = 0.5f;
+    [SerializeField] private float scale = 1.5f;
+    [SerializeField] private float timeOfColourChange = 0.2f;
     [SerializeField] private List<TMP_Text> letters;
     [SerializeField] private List<Image> arrows;
+    [SerializeField] private bool horizontal;
     private int _usedAlphabetLetter;
     private int _activeLetter;
+    private readonly Dictionary<int, int> _lettersAndPosition = new Dictionary<int, int>();
 
     private readonly List<char> _alphabet = new List<char>()
     {
@@ -22,11 +25,28 @@ public class LeaderboardName : MonoBehaviour
         'W', 'X', 'Y', 'Z'
     };
 
+    private void Start()
+    {
+        for (int i = 0; i < letters.Count; i++)
+        {
+            _lettersAndPosition.Add(i, 0);
+        }
+    }
+
     private void Update()
+    {
+        SwitchLetters();
+
+        ChangeLetters(_activeLetter);
+    }
+
+    /// <summary>
+    /// Switch Letter by pressing one button (SPACE), zoom the letter in and show the arrows
+    /// </summary>
+    private void SwitchLetters()
     {
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            _usedAlphabetLetter = 0;
             if (_activeLetter == letters.Count - 1)
             {
                 _activeLetter = 0;
@@ -60,16 +80,18 @@ public class LeaderboardName : MonoBehaviour
                 arrows[i].enabled = false;
             }
         }
-
-        ChangeLetters(_activeLetter);
     }
 
+    /// <summary>
+    /// Change letters on the active letter with two buttons (Q and E)
+    /// </summary>
+    /// <param name="activeLetter"> Which letter in the list is active</param>
     private void ChangeLetters(int activeLetter)
     {
-        
+        _usedAlphabetLetter = _lettersAndPosition[_activeLetter];
         if (Input.GetKeyUp(KeyCode.E))
         {
-            StartColorChange(arrows[activeLetter*2 + 1]);    
+            StartColorChange(arrows[activeLetter * 2 + 1]);
             if (_usedAlphabetLetter == _alphabet.Count - 1)
             {
                 _usedAlphabetLetter = 0;
@@ -79,11 +101,11 @@ public class LeaderboardName : MonoBehaviour
                 _usedAlphabetLetter++;
             }
 
-            LetterFadeIn(_alphabet[_usedAlphabetLetter], activeLetter);
+            LetterShake(_alphabet[_usedAlphabetLetter], activeLetter);
         }
         else if (Input.GetKeyUp(KeyCode.Q))
         {
-            StartColorChange(arrows[activeLetter*2]);    
+            StartColorChange(arrows[activeLetter * 2]);
             if (_usedAlphabetLetter == 0)
             {
                 _usedAlphabetLetter = _alphabet.Count - 1;
@@ -93,27 +115,44 @@ public class LeaderboardName : MonoBehaviour
                 _usedAlphabetLetter--;
             }
 
-            LetterFadeIn(_alphabet[_usedAlphabetLetter], activeLetter);
+            LetterShake(_alphabet[_usedAlphabetLetter], activeLetter);
         }
+
+        _lettersAndPosition[_activeLetter] = _usedAlphabetLetter;
     }
 
-    private void LetterFadeIn(char letterValue, int usedLetter)
+    /// <summary>
+    /// Letter shake effect
+    /// </summary>
+    /// <param name="letterValue"> New value that the letter should have </param>
+    /// <param name="usedLetter"> The letter in the row that we are changing</param>
+    private void LetterShake(char letterValue, int usedLetter)
     {
-        letters[usedLetter].rectTransform.DOShakePosition(shakeTime, new Vector3(shakePower, 0));
+        letters[usedLetter].rectTransform.DOShakePosition(shakeTime,
+            horizontal ? new Vector3(shakePower, 0) : new Vector3(0, shakePower));
 
         letters[usedLetter].text = letterValue.ToString();
     }
 
+    /// <summary>
+    /// Method used to run the coroutine responsible for color (maybe later asset) change
+    /// </summary>
+    /// <param name="image"> The image that is supposed to be changed </param>
     private void StartColorChange(Image image)
     {
         StartCoroutine(ColorChangeCoroutine(image));
     }
 
+    /// <summary>
+    /// Coroutine responsible for color change for the specific amount of seconds
+    /// </summary>
+    /// <param name="image"> The image that is supposed to be changed </param>
+    /// <returns></returns>
     private IEnumerator ColorChangeCoroutine(Image image)
     {
         image.color = Color.green;
 
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(timeOfColourChange);
 
         image.color = Color.white;
     }
