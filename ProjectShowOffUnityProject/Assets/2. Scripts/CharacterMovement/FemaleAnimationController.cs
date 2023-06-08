f908aebfc93c6d3f386322c062728521ded13537
Daniel Born <born.daniel11@gmail.com>
2023-06-08 13:39:17 +0200

    MalecharacterImplemented

---
+++ ProjectShowOffUnityProject/Assets/2. Scripts/CharacterMovement/FemaleAnimationController.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FemaleAnimationController : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator= GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        bool isJumping = Input.GetKey("w");
+        bool isSliding = Input.GetKey("s");
+        bool isRunningRight = Input.GetKey("d");
+        bool isRunningLeft = Input.GetKey("a");
+        bool isStumbelingBackwards = Input.GetKey("f");
+        
+        //JUMPING ANIMATION 
+        if (isJumping)
+        {
+            animator.SetBool("isJumping", true);    
+        }
+        if (!isJumping)
+        {
+            animator.SetBool("isJumping", false);
+        }
+        //SLIDING ANIMATION 
+        if (isSliding)
+        {
+            animator.SetBool("isRunningSlide", true);
+            Debug.Log(isSliding);
+        }
+        if (!isSliding)
+        {
+            animator.SetBool("isRunningSlide", false);
+            Debug.Log(isSliding);
+        }
+        //RunningRight
+        if (isRunningRight)
+        {
+            animator.SetBool("isRunningRight", true);
+            Debug.Log(isRunningRight);
+        }
+        if (!isRunningRight)
+        {
+            animator.SetBool("isRunningRight", false);
+            Debug.Log(isRunningRight);
+        }
+        //Runningleft
+        if (isRunningLeft)
+        {
+            animator.SetBool("isRunningLeft", true);
+        }
+        if (!isRunningLeft)
+        {
+            animator.SetBool("isRunningLeft", false);
+        }
+        //Stumbelingbackwards
+        if (isStumbelingBackwards)
+        {
+            animator.SetBool("isStumbelingBackwards", true);
+        }
+
+        if (!isStumbelingBackwards)
+        {
+            animator.SetBool("isStumbelingBackwards", false);
+        }
+
+    }
+}
