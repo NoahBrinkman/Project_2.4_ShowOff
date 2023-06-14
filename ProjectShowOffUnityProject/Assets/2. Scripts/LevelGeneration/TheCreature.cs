b8be847a099355807d32c2d1662ab23053a04d09
Kamila <kamila.matuszak@list.pl>
2023-06-14 12:01:49 +0200

    LittleCleanup

--- ProjectShowOffUnityProject/Assets/2. Scripts/LevelGeneration/TheCreature.cs
+++ ProjectShowOffUnityProject/Assets/2. Scripts/LevelGeneration/TheCreature.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -12,34 +9,42 @@ public class TheCreature : MonoBehaviour
     private bool _creatureSpawned;
     private int _roadRotation;
     private Vector3 _roadCenter;
-    [SerializeField] private float _heightOffset;
-    [SerializeField] private GameObject _arrow;
-    [SerializeField] private List<GameObject> _creatures;
+    [SerializeField] private float arrowHeightOffset;
+    [SerializeField] private float creatureHeightOffset;
+    [SerializeField] private List<GameObject> arrows;
+    [SerializeField] private List<GameObject> creatures;
     private void OnEnable()
     {
         _roadPoints = GetComponent<RoadPoints>();
         _roadCenter = _roadPoints.RoadCenter;
-        SpawnCreatures();
-        // SpawnText();
-        CreateArrow();
-        Debug.Log(_roadPoints.CreatureSpawn);
+        if (creatures.Count > 0) SpawnCreatures();
+        if (arrows.Count > 0) CreateArrow();
+        
     }
 
     private void SpawnCreatures()
     {
         _roadRotation = ((int)transform.localRotation.eulerAngles.y + 360) % 360;
-        int randomCreature = Random.Range(0, _creatures.Count);
-        GameObject theCreature = Instantiate(_creatures[3], _roadPoints.CreatureSpawn, Quaternion.Euler(0,_roadRotation-90,0));
+        int randomCreature = Random.Range(0, creatures.Count);
+        GameObject theCreature = Instantiate(creatures[randomCreature], new Vector3(_roadPoints.CreatureSpawn.x, _roadPoints.CreatureSpawn.y + creatureHeightOffset, _roadPoints.CreatureSpawn.z), Quaternion.Euler(0,_roadRotation-90,0));
+        theCreature.transform.parent = transform;
     }
 
     private void CreateArrow()
     {
-        // float randomDirection = Random.Range(0, 1);
-        // if (randomDirection == 0)
-        // {
-            GameObject arrowLeft = Instantiate(_arrow, new Vector3(_roadCenter.x, _roadCenter.y + _heightOffset, _roadCenter.z),
+        float randomDirection = Random.Range(0, 1);
+        if (randomDirection == 0)
+        {
+            GameObject arrowLeft = Instantiate(arrows[0], new Vector3(_roadCenter.x, _roadCenter.y + arrowHeightOffset, _roadCenter.z),
                 Quaternion.Euler(0, _roadRotation - 90, 0));
-       // }
+            arrowLeft.transform.parent = transform;
+        }
+        else
+        {
+            GameObject arrowLeft = Instantiate(arrows[1], new Vector3(_roadCenter.x, _roadCenter.y + arrowHeightOffset, _roadCenter.z),
+                Quaternion.Euler(0, _roadRotation - 90, 0));
+            arrowLeft.transform.parent = transform;
+        }
 
     }
 }
